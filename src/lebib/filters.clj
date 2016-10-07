d7c8dfc92f0ea131a1c831c8c7798602ac9889a7
David Schneider <david.schneider@bivab.de>
2016-10-07 16:15:52 +0200

    Simplified filters, using sequences as pair of filter and value again (@bendisposto, @bivab)

--- src/lebib/filters.clj
+++ src/lebib/filters.clj
@@ -8,23 +8,15 @@
 ; map entries from the filter-key to the map of entries that match the criteria
 ; {1998 {cite-key entry}}
 
-(defn- build-kv-filter [[key value]]
-  (fn [db]
-    {(str value)
-     (select-keys db (for  [[k record] db :when (= (key record) value)] k))}))
+(defn- build-year-filter [value]
+  (fn [db] [(keyword (str value)) (filter #(= value (:year %)) db)]))
 
 (defn- build-author-filter [author]
-  (let [pred (fn [s] (.contains (lower-case s) (name author)))]
+  (let [pred (fn [s] (.contains (lower-case s) (lower-case (name author))))]
     (fn [db]
-      {author
-       (select-keys db (for [[k v] db :when (some pred (:author v))] k))})))
+      [author (filter #(some pred (:author %)) db)])))
 
-(defn- build-keyword-filter [kw]
-  (let [lkw (lower-case (name kw))
-        keywords (fn [v] (split (lower-case (get v :stupskeywords "")) #","))]
-    (fn [db]
-        {lkw
-         (select-keys db (for [[k v] db :when (some #{lkw} (keywords v))] k))})))
+(defn- build-keyword-filter [kw] (fn [db] [kw (filter #(some #{kw} (:stupskeywords %)) db)]))
 
 (def ^{:private true} authors [:leuschel :bendisposto :schneider :dobrikov
                                :hansen :krings :ladenberger :witulski :clark
@@ -33,13 +25,12 @@
                                :hager :hallerstede :hudson :jastram :luo
                                :plagge :rigo :samia :spermann :weigelt
                                :wiegard])
-(def ^{:private true} keywords [:advance :prob :pyb])
-(def ^{:private true} kv-pairs (mapv (fn [x] [:year x]) (range 1998
-                                                               (.getValue (java.time.Year/now)))))
+(def ^{:private true} keywords [:advance :prob :pyb :tla])
 
 (def ^{:private true} author-rules (map build-author-filter authors))
-(def ^{:private true} kv-rules (map build-kv-filter kv-pairs))
+(def ^{:private true} year-rules (map build-year-filter (range 1990 (.getValue (java.time.Year/now)))))
 (def ^{:private true} keyword-rules (map build-keyword-filter keywords))
-(def ^{:private true} all-rule [(fn [db] {:all db})])
+(def ^{:private true} all-rule [(fn [db] [:all db])])
 
-(def rules (concat all-rule author-rules kv-rules keyword-rules))
+(def ^{:private true} rules (concat all-rule author-rules year-rules keyword-rules))
+(defn filtered [db] (filter (comp seq second) ((apply juxt rules) db)))
