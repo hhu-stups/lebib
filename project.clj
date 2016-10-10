d934e4708433bf4cb2f59b09fe6550499264503a
David Schneider <david.schneider@bivab.de>
2016-10-10 10:33:04 +0200

    Use latest version of jbibtex (github) through jitpack

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.3.5"]
+  :repositories  [["jitpack" "https://jitpack.io"]]
+  :dependencies [[clj-tagsoup "0.3.0"  :exclusions  [org.clojure/clojure]]
+                 [com.github.jbibtex/jbibtex "-SNAPSHOT"]
                  [hiccup "1.0.5"]
-                 [clj-tagsoup "0.3.0"  :exclusions  [org.clojure/clojure]]
-                 [org.jbibtex/jbibtex "1.0.15"]]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot lebib.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
