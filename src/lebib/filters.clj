9000afd8e2c3c64ec6181fbdedd3a8f38d2c1a17
David Schneider <david.schneider@bivab.de>
2018-01-08 09:54:41 +0100

    Added publication filter for Joshua

--- src/lebib/filters.clj
+++ src/lebib/filters.clj
@@ -24,7 +24,7 @@
                                :craig :cuni :elbeshausen :fontaine :fritz
                                :hager :hallerstede :hudson :jastram :luo
                                :plagge :rigo :samia :spermann :weigelt
-                               :wiegard])
+                               :wiegard :schmidt])
 (def ^{:private true} keywords [:advance :prob :pyb :tla])
 
 (def ^{:private true} author-rules (map build-author-filter authors))
